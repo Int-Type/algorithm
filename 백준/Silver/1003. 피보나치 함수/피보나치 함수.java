4f8d008f3d2267dd67d1938e372279eab477e862
Int-Type <inttype96@gmail.com>
2024-12-02 20:37:18 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 15948 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] zero = new int[41];
+        int[] one = new int[41];
+
+        zero[0] = 1;
+        one[1] = 1;
+
+        for (int i = 2; i <= 40; i++) {
+            zero[i] = zero[i - 1] + zero[i - 2];
+            one[i] = one[i - 1] + one[i - 2];
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(zero[n] + " " + one[n]);
+        }
+    }
+}
