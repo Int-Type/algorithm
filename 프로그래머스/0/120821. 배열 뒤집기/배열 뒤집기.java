7f9609d9f554d31c1bd3adc6e6688f61fbd5532a
Int-Type <inttype96@gmail.com>
2024-12-09 00:15:34 +0900

    [level 0] Title: 배열 뒤집기, Time: 2.79 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i = num_list.length - 1; i >= 0; i--) {
+            answer.add(num_list[i]);
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
