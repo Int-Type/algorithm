ca1ec7f4f97dcc4999009f7cdec0ed95f186607c
Int-Type <inttype96@gmail.com>
2024-12-21 22:31:21 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int i = 1;
+        int answer = 1;
+        
+        while(answer <= n) {
+            i++;
+            answer *= i;
+        }
+        
+        return i - 1;
+    }
+}
