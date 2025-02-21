a29d60997b400381d711366ec3b9b171d11deabd
Int-Type <inttype96@gmail.com>
2025-02-21 20:38:53 +0900

    [Silver III] Title: N과 M (3), Time: 416 ms, Memory: 68520 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int n, m;
+    private static int[] arr;
+    private static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int cnt) {
+        if (cnt == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            arr[cnt] = i;
+            dfs(cnt + 1);
+        }
+    }
+}
