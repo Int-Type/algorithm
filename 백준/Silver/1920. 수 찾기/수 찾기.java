3c60f4ae972cce541db85284a825f2686060c944
Int-Type <inttype96@gmail.com>
2025-04-22 23:07:54 +0900

    [Silver IV] Title: 수 찾기, Time: 516 ms, Memory: 58716 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,23 +9,44 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        HashSet<Integer> set = new HashSet<>();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr1 = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
 
-        int m = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] arr2 = new int[M];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            if (set.contains(Integer.parseInt(st.nextToken()))) {
-                sb.append(1).append("\n");
-            } else {
-                sb.append(0).append("\n");
-            }
+        for (int i = 0; i < M; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr1);
+
+        for (int i : arr2) {
+            sb.append(searchArr(i, arr1)).append("\n");
+        }
+
         System.out.println(sb);
+    }
+
+    private static int searchArr(int n, int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = (start + end) / 2;
+
+        while (start <= end) {
+            if (arr[mid] == n) return 1;
+
+            if (n > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            mid = (start + end) / 2;
+        }
+        return 0;
     }
 }
