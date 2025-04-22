e89d5fdc25003db8cd0e7025d84678fbced7d11e
Int-Type <inttype96@gmail.com>
2025-04-22 23:09:01 +0900

    [Silver II] Title: 나무 자르기, Time: 676 ms, Memory: 167924 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int start = 0, result = 0;
+        int end = arr[arr.length-1];
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            long total = 0;
+            for (int i : arr) {
+                if (i > mid) total += i - mid;
+            }
+
+            if (total >= M) {
+                result = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
